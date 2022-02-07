bd1549ddf0897d2f889d84c4860d4028d7c27ec1
wangxd01 <wangxd01@mingyuanyun.com>
2022-02-07 16:44:00 +0800

    httpie

---
+++ src/main.rs
@@ -0,0 +1,135 @@
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use colored::*;
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
+use std::env::args;
+use std::{collections::HashMap, str::FromStr};
+
+#[derive(Parser, Debug)]
+struct Opts {
+    #[clap(subcommand)]
+    subsumed: Subcommand,
+}
+
+#[derive(Parser, Debug)]
+enum Subcommand {
+    Get(Get),
+    Post(Post),
+}
+
+#[derive(Parser, Debug)]
+struct Get {
+    url: String,
+}
+
+#[derive(Parser, Debug)]
+struct Post {
+    #[clap(parse(try_from_str = parse_url))]
+    url: String,
+    #[clap(parse(try_from_str = parse_key_val))]
+    body: Vec<KvPair>,
+}
+
+fn parse_key_val(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?.to_string()),
+            v: (split.next().ok_or_else(err)?.to_string()),
+        })
+    }
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+
+    Ok(s.into())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    let mut headers = header::HeaderMap::new(); // 为我们的 HTTP 客户端添加一些缺省的 HTTP 头
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subsumed {
+        Subcommand::Get(ref args) => get(client, args).await?,
+        Subcommand::Post(ref args) => post(client, args).await?,
+    };
+    Ok(result)
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+    print!("\n");
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        } // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+/// 打印整个响应
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+/// 将服务器返回的 content-type 解析成 Mime 类型
+
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
